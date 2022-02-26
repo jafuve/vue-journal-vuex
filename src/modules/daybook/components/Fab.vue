b48ba8643c4f2c88236a2a9f7dca1f126b24ff6b
Jairo Fuentes <ing.jairofuentesv@gmail.com>
2022-02-26 10:54:33 -0600

    Firs commit, desinged frontend, and vuex config

---
+++ src/modules/daybook/components/Fab.vue
@@ -0,0 +1,28 @@
+<template>
+    <button class="btn btn-primary">
+        <i class="fa fa-2x " :class="icon"></i>
+    </button>
+</template>
+
+<script>
+export default {
+    name: 'FabComponent',
+    props: {
+        icon: {
+            type: String,
+            default: 'fa-plus'
+        }
+    }
+}
+</script>
+
+<style class="scss" scoped>
+button{
+    position: fixed;
+    bottom: 20px;
+    right: 20px;
+    width: 80px;
+    height: 80px;
+    border-radius: 100%;
+}
+</style>
